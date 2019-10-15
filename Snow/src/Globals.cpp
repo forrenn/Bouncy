8936cd87d1b3b1cf51bb4b1a56a86c6a6920ed09
forren <forren@protonmail.ch>
2019-10-16 01:45:01 +0500

    Added SPEED_CUTOFF, removed SHAPES_TO_DELETE from globals

--- Snow/src/Globals.cpp
+++ Snow/src/Globals.cpp
@@ -1,7 +1,7 @@
 #include "Globals.h"
 
 std::vector<std::shared_ptr<Shape>> SHAPES;
-std::vector<size_t> SHAPES_TO_DELETE;
 
 int SCREEN_WIDTH;
 int SCREEN_HEIGHT;
+const double SPEED_CUTOFF = 1e-9;
--- Snow/src/Globals.h
+++ Snow/src/Globals.h
@@ -3,7 +3,7 @@
 #include <memory>
 struct Shape;
 extern std::vector<std::shared_ptr<Shape>> SHAPES;
-extern std::vector<size_t> SHAPES_TO_DELETE;
 
 extern int SCREEN_WIDTH;
 extern int SCREEN_HEIGHT;
+extern const double SPEED_CUTOFF;
