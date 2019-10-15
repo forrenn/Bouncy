55216b3a63758d0b8cf7126df8357dbe7b98344e
forren <forren@protonmail.ch>
2019-10-15 14:07:52 +0500

    Optimized and disable collision detection

--- Snow/src/Shape.cpp
+++ Snow/src/Shape.cpp
@@ -7,7 +7,7 @@ void Shape::draw(SDL_Surface * surf)
 
 void Shape::updatePos(double dt, std::vector<std::unique_ptr<Shape>>& shapes, double maxX, double maxY)
 {
-	vy += 9.81*dt; //gravity
+	vy += 98.1*dt; //gravity
 	double xCopy = x;
 	double yCopy = y;
 	this->x += vx * dt;
@@ -26,21 +26,22 @@ void Shape::updatePos(double dt, std::vector<std::unique_ptr<Shape>>& shapes, do
 	}
 	else prev_y = yCopy;
 
-	for (size_t i = 0; i < shapes.size(); ++i) //Circle-Circle collision, please refactor in the future (move to Circle)
+	/*for (size_t i = 0; i < shapes.size(); ++i) //Circle-Circle collision, please refactor in the future (move to Circle)
 	{
 		if (this == shapes[i].get()) continue; //skip self collision
 		double dx = shapes[i]->x - x;
 		double dy = shapes[i]->y - y;
-		double dist = sqrt(dx*dx + dy * dy);
+		double dist2 = dx * dx + dy * dy;
 		double r_sum = size + shapes[i]->size;
-		if (dist <= r_sum)
+		r_sum *= r_sum;
+		if (dist2 <= r_sum)
 		{
 			vx = -vx;
 			vy = -vy;
 			shapes[i]->vx = -shapes[i]->vx;
 			shapes[i]->vy = -shapes[i]->vy;
 		}
-	}
+	}*/
 }
 
 void Shape::update(double dt, std::vector<std::unique_ptr<Shape>>& shapes, double maxX, double maxY)
