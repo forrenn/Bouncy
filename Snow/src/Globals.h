c8bccbb7e8272f08797f644754afb50a0c5a9449
forren <forren@protonmail.ch>
2019-10-17 01:28:56 +0500

    Added default screen width and height

--- Snow/src/Globals.h
+++ Snow/src/Globals.h
@@ -6,8 +6,13 @@ extern std::vector<std::shared_ptr<Shape>> SHAPES;
 
 extern int SCREEN_WIDTH;
 extern int SCREEN_HEIGHT;
+extern const int DEFAULT_SCREEN_WIDTH;
+extern const int DEFAULT_SCREEN_HEIGHT;
+
 extern const double SPEED_CUTOFF;
 extern double FPS_COUNTER_REFRESH_INTERVAL;
 extern int CIRCLE_SPLIT_MULT;
 extern double GRAVITY;
 extern const double BENCHMARK_TIME_STEP;
+extern const int BENCHMARK_DEFAULT_SCREEN_WIDTH;
+extern const int BENCHMARK_DEFAULT_SCREEN_HEIGHT;
